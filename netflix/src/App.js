45ae3c0ad574f55670de483c59d718eddb1ef63b
“shamnad” <hamzashamnad123@gmail.com>
2023-12-29 19:35:26 +0530

    29

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Banner from './Components/Banner/Banner';
+import Rowpost from './Components/Rowpost/Rowpost';
 
 function App() {
   return (
     <div className='App'>
      <Navbar/>
      <Banner/>
+     <Rowpost/>
     </div>
   );
 }
