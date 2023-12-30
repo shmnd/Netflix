1a8fe8ddc84322099a44877d6afed57758ad3398
“shamnad” <hamzashamnad123@gmail.com>
2023-12-30 21:36:01 +0530

    random image

--- netflix/src/Components/Banner/Banner.jsx
+++ netflix/src/Components/Banner/Banner.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
+import  axios from '../../axios'
+import { API_KEY,ImageUrl } from '../../constants/constants'
 
 function Banner() {
+  const [movie, setMoive] = useState()
+
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0])
+      const randomImg=response.data.results
+      const randomIndex = Math.floor(Math.random() * randomImg.length);
+      console.log(randomIndex) 
+      setMoive(response.data.results[randomIndex])
+    })
+  }, [])
+
   return (
-    <div className='banner'>
+    <div style={{backgroundImage:`url(${movie ? ImageUrl+movie.backdrop_path: ''})`}} className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie name</h1>
+            <h1 className='title'>{movie ? movie.title : ''}</h1>
             <div className='banner_buttons'>
                 <button className='button'>play</button>
                 <button className='button'>my list</button>
             </div>
-            <h1 className='description'>ABCDEFGHIJKLMNOPQRSTUVWXYZ</h1>
+            <h1 className='description'>{movie ? movie.overview : ''}</h1>
         </div>
         <div className="fade_bottom"></div>
     </div>
---
+++ netflix/src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseURL='https://api.themoviedb.org/3';
+export const API_KEY='405cb1382b0436939e018193d583c6f7';
+export const ImageUrl='https://image.tmdb.org/t/p/original'
