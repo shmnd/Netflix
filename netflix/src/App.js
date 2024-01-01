f962bd7fdb97136cfe7ccd23267f0f6c99dce01c
“shamnad” <hamzashamnad123@gmail.com>
2024-01-01 11:37:21 +0530

    posters row

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import{original,action,comdey,trending,documentaries,horrorMovies} from './urls'
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Banner from './Components/Banner/Banner';
@@ -10,8 +10,15 @@ function App() {
     <div className='App'>
      <Navbar/>
      <Banner/>
-     <Rowpost/>
-     <Rowpost/>
+     <Rowpost url={original} title='Netflix Originals'/>
+     <Rowpost url={trending} title='Trending' isMedium/>
+     <Rowpost url={horrorMovies} title='HorrorMovies' isSmall/>
+     <Rowpost url={action} title='Action' isSmall/>
+     <Rowpost url={documentaries} title='Documentaries' isSmall/>
+     <Rowpost url={comdey} title='Comedy' isSmall/>
+
+     HorrorMovies
+
  
     </div>
   );
--- netflix/src/Components/Rowpost/Rowpost.jsx
+++ netflix/src/Components/Rowpost/Rowpost.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import './Rowpost.css'
 import axios from '../../axios'
-import { API_KEY,ImageUrl } from '../../constants/constants'
-function Rowpost() {
+import {ImageUrl } from '../../constants/constants'
+function Rowpost(props) {
   const [movies, setMovies] = useState([])
 
   useEffect(() => {
-    axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then(response=>{
+    axios.get(props.url).then(response=>{
       console.log(response)
       setMovies(response.data.results)
     }).catch(err=>{
@@ -18,10 +18,11 @@ function Rowpost() {
   return (
   
     <div className='row'>
-      <h2>Netflix Original</h2>
+      <h2>{props.title}</h2>
       <div className='posters'>
         {movies.map((obj)=>
-          <img className='poster' src={`${ImageUrl+obj.backdrop_path}`} alt='' />
+          <img alt='poster' className={props.isSmall ? 'smallposter' : props.isMedium ? 'mediumposter': 'poster'} 
+          src={`${ImageUrl+obj.backdrop_path}`} />
         )}
       </div>
     </div>
